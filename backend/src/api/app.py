66316a0ab2b52a917b655c528014432f3f3da942
Alonso Reyes <alonso.reyes@houm.com>
2024-02-26 11:46:51 -0300

    chore: allow all cors

--- backend/src/api/app.py
+++ backend/src/api/app.py
@@ -1,9 +1,19 @@
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.routes.visits import router as visits_router
 from src.settings import settings
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Init FastAPI
 app = FastAPI(docs_url="/docs", redoc_url=None)
