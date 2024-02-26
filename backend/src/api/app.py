b0957efe554d3ce5db82c5da0e066b6e2de1dca2
Alonso Reyes <alonso.reyes@houm.com>
2024-02-26 13:00:54 -0300

    allow cors

--- backend/src/api/app.py
+++ backend/src/api/app.py
@@ -4,19 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes.visits import router as visits_router
 from src.settings import settings
 
-app = FastAPI()
+app = FastAPI(docs_url="/docs", redoc_url=None)
 
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 # Init FastAPI
-app = FastAPI(docs_url="/docs", redoc_url=None)
 base_router = APIRouter(prefix=settings.PREFIX_URL)
 base_router.include_router(visits_router)
 app.include_router(base_router)
